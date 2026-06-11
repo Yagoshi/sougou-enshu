726abac5e9d9946546cc16f7c543668caaa733ef
admin <test@example.com>
2026-06-11 14:13:03 +0900

    カート機能の実装

--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.main, name='main'), # S01
-    path('search_result/', views.searchResult, name='searchResult'), # S02
-    path('item/<int:item_id>/', views.itemDetail, name='itemDetail'), # S03
-    path('cart/', views.cart, name='cart'), # S04
+    path('', views.main, name='main'),
+    path('search_result/', views.searchResult, name='searchResult'),
+    path('item/<int:item_id>/', views.itemDetail, name='itemDetail'),
+    path('cart/', views.cart, name='cart'),
+    path('add_to_cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
